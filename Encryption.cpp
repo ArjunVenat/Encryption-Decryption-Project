8aeaa867d347e59144d5b855916881e3ee66614c
ArjunVenat <34625035+ArjunVenat@users.noreply.github.com>
2018-03-29 15:00:41 -0400

    Add files via upload

---
+++ Encryption.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	
+	//Allowing the user to input the message that is going to be encrypted
+	
+	int gg;
+	char storage[1000];
+	int part2[1000];
+	string abcd;
+	cout << "Enter the Message" << endl;
+	getline(cin, abcd);
+	int i = abcd.length();
+	cout << endl;
+
+//Converts the string into several characters
+
+	istringstream characters(abcd);
+	
+	//Accounts for Fillers
+	int x = i;
+	int y = 3 - (i % 3);
+	i = x + y;
+	
+	//For loop which converts each character into a number which is then displayed. 
+
+	for (int O = 1; O <= i; O++) {
+		characters >> noskipws >> storage[O]; //Does not skip spaces and the character is stored into an character array
+		part2[O] = storage[O]; //The element from the character array is stored into an integer array
+
+		if (O > (i - y)) {
+			for (int c = 1; c <= y; c++)
+			{
+				part2[O] = 120;
+				O++;
+
+				//The Fillers are going to be 1
+			}
+			break;
+		}
+
+		if (storage[O] == '.')
+			part2[O] = 117;
+
+		else if (storage[O] == ' ')
+			part2[O] = 1;
+		else {
+			int a = part2[O];
+			int b = (part2[O]) - 64;
+
+			for (int j = 1; j <= b; j++)
+			{
+				a = a + 1;
+			}
+
+			part2[O] = a;
+			gg = O;
+		}
+	}
+
+
+//Random 3X3 Matrix (Key) to multiply the plain text with to convert to cipertext
+
+	int random[3][3];
+	int w, q;
+
+	srand((int)time(0));
+
+	for (w = 0; w < 3; w++)
+		for (q = 0; q < 3; q++) {
+			random[w][q] = (rand() % 10) - 5;
+			cout << random[w][q] << "  ";
+			if (q == 2)
+				cout << endl;
+		}
+	cout << random[0][0] << endl;
+	
+	//For loop which stores each 3 terms down by rows in a separate column of one matrix
+
+	cout << endl;
+	int ptm[7][300];
+	// "ptm" = Plain Text Matrix (1x3 matrices)
+
+	int h = 1;
+	for (int c = 1; c <= (i / 3); c++) {
+		
+		for (int r = 1; r <= 3; r++) {
+			int a = part2[h];
+			ptm[r][c] = a;
+			h++;
+			cout << ptm[r][c] << "  ";
+			}
+		cout << endl;
+	}
+
+	cout << ptm[1][1] << endl;
+	//Multiplies the Key matrix with the Plain Text Matrix to create ciphertext
+	
+	// "ctm" = Ciper Text Matrix
+	int ctm[7][300];
+
+	for (int c = 1; c <= (i / 3); c++) {
+		c++;
+			for (int r = 0; r < (i / 3); r++) {
+				for (int q = 0; q < (i / 3); q++) {
+					int a = random[r][q] * ptm[r + 1][c];
+
+
+				}
+				
+
+			}
+			
+
+	}
+
+	
+
+
+
+	system("pause");
+ }
+
+
+
+
