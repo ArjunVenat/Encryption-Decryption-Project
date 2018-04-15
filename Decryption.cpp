e2dcff2af077dbe7337684ae29e9e6eb45ef490a
ArjunVenat <34625035+ArjunVenat@users.noreply.github.com>
2018-04-14 23:59:16 -0400

    Update Decryption.cpp

--- Decryption.cpp
+++ Decryption.cpp
@@ -19,29 +19,44 @@ void cofactorMatrix(int[3][3], float[3][3], float[2][2]);
 
 int main()
 {
-	int random[3][3];
-	float C[3][3], Adjoint[3][3], Inverse[3][3], ctm[3][500], ctm3x1[3][1], Decoded[3][700];
+
+	float C[3][3], Adjoint[3][3], Inverse[3][3], ctm3x1[3][1], Decoded[3][700];
+	int Decoded2[3][700];
 	float determinant[3][5];
 	float temp[2][2];
 	float determinantA;
 	char message[3][700];
+	int b;
+	cout << "Length of the Message: "; //Remove after integration
+	cin >> b;
+	cout << endl;
 
 	srand((int)time(0));
 
 	//Random 3X3 Matrix (Key) to multiply the plain text with to convert to cipertext
+	//The job of the integrator is to deliver the values of these matrices (random Key Matrix and Cipher Text Matrix) from the encryption file to this Decryption File
+
+	int random[3][3];
+	float ctm[3][500];
+
+
 	cout << "Key Matrix: \n" << endl;
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
 		{
 			cin >> random[i][j];
-			
+		}
+	cout << endl;
 
-			//random[i][j] = (rand() % 20) - 10;
-
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+		{
 			cout << random[i][j] << "  ";
 			if (j == 2)
 				cout << endl;
 		}
+	
+	cout << endl;
 
 //Co-factor Matrix
 	cofactorMatrix(random, C, temp);
@@ -69,7 +84,7 @@ int main()
 		}
 	cout << endl;
 
-	//	| A | = a11a22a33 + a12a23a31 + a13a21a32 – a31a22a13 – a32a23a11 – a33a21a12
+	//	| A | = a11a22a33 + a12a23a31 + a13a21a32 â€“ a31a22a13 â€“ a32a23a11 â€“ a33a21a12
 	determinantA = determinant[0][0] * determinant[1][1] * determinant[2][2] +
 		determinant[0][1] * determinant[1][2] * determinant[2][3] +
 		determinant[0][2] * determinant[1][3] * determinant[2][4] -
@@ -77,7 +92,7 @@ int main()
 		determinant[2][1] * determinant[1][2] * determinant[0][3] -
 		determinant[2][2] * determinant[1][3] * determinant[0][4];
 
-
+	
 	cout << endl;
 	cout << "\nInverse Matrix: \n" << endl;	//Inverse Matrix
 	for (int i = 0; i < 3; ++i)
@@ -89,19 +104,23 @@ int main()
 				cout << "\n";
 		}
 
-	//Random Ciphertext Matrix
-	cout << "\Ciphertext Matrix: \n" << endl;
+	cout << endl;
+	// Ciphertext Matrix from the encryption file
+
+	//In the for loop parameters, the three msut be changed. The three is hard coded for now but it must be changed to ( number of columns of cipher text matrix divided by three)
+
+	cout << "Ciphertext Matrix: \n" << endl;
 	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < b/3; j++)
 		{
 			cin >> ctm[i][j];
 
-			cout << ctm[i][j] << "  ";
-			if (j == 2)
+			if (j == 3)
 				cout << endl;
 		}
 
-	int b = 9;
+	//b must be the number of columns of the cipher text matrix delivered from the Encryption File
+
 
 	for (int i = 0; i < (b / 3); i++)
 		for (int j = 0; j < 3; j++)
@@ -110,7 +129,13 @@ int main()
 			for (int k = 0; k < 3; k++)
 			{
 				a = a + Inverse[j][k] * ctm[k][i];
+			
 			}
+			if (a < 0) 
+			{ 
+				a = 0;
+			}
+
 			Decoded[j][i] = a;
 		}
 
@@ -118,8 +143,7 @@ int main()
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < (b / 3); j++)
 		{
-			cout << Decoded[i][j] << " ";
-			if (j == 2)
+			if (j == (b/3) -1)
 				cout << endl;
 		}
 
@@ -128,15 +152,16 @@ int main()
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < (b / 3); j++)
 		{
-			Decoded[i][j] = round(Decoded[i][j]);
+					Decoded[i][j] = round(Decoded[i][j]);
+					if (j == (b / 3) - 1)
+						cout << endl;
 		}
 
 	cout << "\nAccurate: " << endl;
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < (b / 3); j++)
 		{
-			cout << Decoded[i][j] << " ";
-			if (j == 2)
+			if (j == (b/3) - 1)
 				cout << endl;
 		}
 	cout << endl;
@@ -144,37 +169,42 @@ int main()
 	for (int i = 0, a; i < 3; i++)
 		for (int j = 0; j < (b / 3); j++)
 		{
-			if (Decoded[i][j] == 117)
+
+			if (Decoded[i][j] == 256)
 				message[i][j] = '.';
 
-			if (Decoded[i][j] == 1)
+			else if (Decoded[i][j] == 1)
 				message[i][j] = ' ';
-
-			else
-			{
+						
+			else {
+				 
 				a = Decoded[i][j];
 
 				a -= 64;
 				a /= 2;
-				//		cout << a
+				
 				a += 64;
 
+
 				message[i][j] = a;
-				cout << a << " ";
-				if (j == 2)
+
+				if (j == (b / 3) - 1)
 					cout << endl;
+
 				// The formula is 64 + 2((part 2 value) - 64)
 				// For example, a = 65 in ASCII. A becomes 64 + 2(65-64) or 66
 			}
 		}
 
 	cout << "\nMessage: " << endl;
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < (b / 3); j++)
+	for (int i = 0; i < (b/3); i++)
+		for (int j = 0; j < 3; j++)
 		{
 			cout << message[j][i];
 		}
 	cout << endl;
 
-	system("pause");
+	int goodbye;
+	cin >> goodbye;
+	return 0;
 }
