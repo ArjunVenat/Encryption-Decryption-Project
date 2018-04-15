e220dabc61c62981920dfa240d94022d45e627d2
ArjunVenat <34625035+ArjunVenat@users.noreply.github.com>
2018-04-15 00:00:54 -0400

    Update Encryption.cpp

--- Encryption.cpp
+++ Encryption.cpp
@@ -12,8 +12,8 @@
 using namespace std;
 
 int main() {
-
-
+	
+	int goodbye;
 
 	//Allowing the user to input the message that is going to be encrypted
 
@@ -25,14 +25,14 @@ int main() {
 	getline(cin, abcd);
 
 	//After entering the message, a part of the song below will be played
-	PlaySound("C:\\Users\\arjun\\Downloads\\2001_ A Space Odyssey Theme Song (Also sprach Zarathustra) (1).wav", NULL, SND_SYNC);
-	//The file must be downloaded in order to run the program
 	
+	PlaySound("C:\\Users\\arjun\\Downloads\\Rick Astley - Never Gonna Give You Up (Video) (online-audio-converter.com).wav", NULL, SND_ASYNC);
+
 	int i = abcd.length();
 	cout << endl;
 
 	//Converts the string into several characters, and each character is stored into an array
-
+	
 	istringstream characters(abcd);
 
 	for (int h = 1; h <= i; h++) {
@@ -52,54 +52,77 @@ int main() {
 		y = 3 - (i % 3);
 		i = i + y;
 }
-
-		
+			
 		
 	//Function which converts each character into a number which is then displayed. 
-	charconverter(i, storage, part2, y);
-
-	//Random 3X3 Matrix (Key) to multiply the plain text with to convert to cipertext
-
-	int random[3][3];
-	int w, q;
-
-	srand((int)time(0));
-
-	for (w = 0; w < 3; w++)
-		for (q = 0; q < 3; q++) {
-		random[w][q] = (rand() % 20) - 10;
-		cout << random[w][q] << "  ";
-		if (q == 2)
-		cout << endl;
-	}
-
-
-	cout << endl;
-
-
-	//Plain Text Matrix
-	int ptm[7][300];
-
-
-	//Function which converts the Plaintext into an array and is then organized into 3x3 arrays.
-
-	arrangement1x3(i, part2, ptm);
-	cout << endl;
-
-	//Function which multiplies the Key matrix with the Plain Text Matrix to create ciphertext
-
-
-	//Cipher Text matrix
-	int ctm[7][300];
-
-	matrixmult(i, random, ptm, ctm);
-
-	//For dramatic effect, the rest of the song will be played at this point
-
-	PlaySound("C:\\Users\\arjun\\Downloads\\2001_ A Space Odyssey Theme Song (Also sprach Zarathustra) (7).wav", NULL, SND_SYNC);
 	
 
-	//End of the Encyption Program
+		charconverter(i, storage, part2, y);
 
-	return 0;
-}
+		//Random 3X3 Matrix (Key) to multiply the plain text with to convert to cipertext
+
+		int random[3][3];
+
+				cout << "Key Matrix\n" << endl;
+			int w;
+			int q;
+			
+			srand((int)time(0));
+
+		
+				for (w = 0; w < 3; w++)
+					for (q = 0; q < 3; q++) {
+						random[w][q] = (rand() % 20) - 10;
+			}
+				
+
+			for (w = 0; w < 3; w++)
+				for (q = 0; q < 3; q++) {
+					cout << random[w][q] << " ";
+					if (q == 2)
+						cout << endl;
+				}
+
+			cout << endl;
+
+			cout << "OR" << endl;
+			cout << endl;
+			
+			
+				for (w = 0; w < 3; w++)
+					for (q = 0; q < 3; q++) {
+
+						cout << random[w][q] << "  ";
+
+					}
+						
+
+		cout << endl;
+
+		cout << endl;
+		
+		cout << "Character Converter" << endl;
+	
+		//Plain Text Matrix
+		int ptm[7][300];
+
+
+		//Function which converts the Plaintext into an array and is then organized into 3x3 arrays.
+
+		arrangement1x3(i, part2, ptm);
+		cout << endl;
+
+		//Function which multiplies the Key matrix with the Plain Text Matrix to create ciphertext
+			
+
+		//Cipher Text matrix
+		int ctm[7][300];
+
+		matrixmult(i, random, ptm, ctm);
+
+
+		//End of the Encyption Program
+
+		cin >> goodbye;
+		return 0;
+};
