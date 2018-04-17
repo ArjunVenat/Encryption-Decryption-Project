e7040a6efd5410af35e837ccbef67adcaaedd65c
ArjunVenat <34625035+ArjunVenat@users.noreply.github.com>
2018-04-17 00:42:15 -0400

    Update Encryption-Decryption Project.cpp

--- Encryption-Decryption Project.cpp
+++ Encryption-Decryption Project.cpp
@@ -3,7 +3,10 @@
 #include <string>
 #include <fstream>
 #include "Encryption Header.h"
+#include "Encryption Header 2.h"
 #include "Decryption Header.h"
+#include "Decryption Header 2.h"
+#include "Intro Sequence.h"
 #include <stdlib.h>
 #include <cmath>
 #include <ctime>
@@ -18,9 +21,9 @@ using namespace std;
 int main()
 {
 
-int random[3][3];
-int ctm[3][700];
-int i = 1, b = 1;
+	int random[3][3];
+	int ctm[3][700];
+	int i = 1, b = 1;
 
 	char input[80];
 	char temp[80];
@@ -28,44 +31,52 @@ int i = 1, b = 1;
 	char answer;
 	char choice;
 
+	//IntroSequence();
 
-		cout << "\t\nWELCOME TO THE ENCRYPTION AND DECRYPTION PROJECT!" << endl;
-		cout << "Would you like to Encrypt {E/e} or Decrypt {D/d}" << endl;
+	cout << "\t\nFILE PATH: " << endl;
 
+	cout << "Enter filename: ";
+	getline(cin, filename);    //allows for spaces to be read as spaces
+							   //use getline(cin, filename) for strings only
+
+	cout << "\nCreating FILE " << filename << endl;
+	filename = filename.append(".txt"); //forces the file to be a text file
+
+										//ofstream is for outputing. it is used to open the file
+	ofstream Outfile(filename, ios::app); //app is appending    //Outline is a HANDLE that is used to output
+										  //it is an object now
+	if (Outfile.fail())    //if the file is open
+	{
+		cout << "Error" << endl;
+		exit(1);
+	}
+	cin.ignore();
+
+	cout << endl;
+	int tally = 0;
+
+	cout << "\t\nWELCOME TO THE ENCRYPTION AND DECRYPTION PROJECT!" << endl;
+	cout << "Would you like to Encrypt {E/e} or Decrypt {D/d}" << endl;
+
+
+	
 		cin >> choice;
 
-		if (choice == 'e' || choice == 'E')
-		{
+	
+		if (choice == 'e' || choice == 'E') {
 			Encryption(random, ctm, i);
-			cout << i << endl;
+
+			cout << "The Length of the Message including fillers: " << i << endl;
 			Decryption(i, random, ctm);
-
 		}
-		
-		if (choice == 'd' || choice == 'D')
-		{
+	
+		if (choice == 'd' || choice == 'D') {
+			Decryption(i, random, ctm);
+			return 0;
 		}
 
-		cout << "\t\nFILE PATH: " << endl;
+			
 
-		cout << "Enter filename: ";
-		getline(cin, filename);    //allows for spaces to be read as spaces
-								   //use geline(cin, filename) for strings only
-
-		cout << "\nCreating FILE " << filename << endl;
-		filename = filename.append(".txt"); //forces the file to be a text file
-
-											//ofstream is for outputing. it is used to open the file
-		ofstream Outfile(filename, ios::app); //app is appending    //Outline is a HANDLE that is used to output
-											  //it is an object now
-		if (Outfile.fail())    //if the file is open
-		{
-			cout << "Error" << endl;
-			exit(1);
-		}
-		cin.ignore();
-
-		cout << endl;
 		cout << "\nENCRYPTION: " << endl;
 
 		for (int x = 0; x < 10; ++x)
@@ -74,23 +85,31 @@ int i = 1, b = 1;
 			Sleep(100);
 		}
 
+		Outfile << "Length of Message Including Fillers: ";
+			Outfile << i << endl;
+
 		cout << "Key Matrix: " << endl;
 		for (int c = 0; c < 3; ++c)
 			for (int r = 0; r < 3; ++r)
 			{
-				cout << random[c][r] << " ";
+				Outfile << random[c][r] << " ";
 				if (r == 2)
-					cout << endl;
+					Outfile << endl;
 			}
-		cout << endl;
-		cout << "CTM Matrix: " << endl;
-		for (int c = 0; c < 3; c++)
-			for (char r = 0; r != '\r'; r++)
+		
+		
+		Outfile << "CTM Matrix: " << endl;
+
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < i/3; c++)
 			{
-				cout << ctm[c][r] << " ";
-				if (r == (i / 3) - 1)
-					cout << endl;
+				Outfile << ctm[r][c] << " ";
+
+				
+				
 			}
+			Outfile << "\n";
+		}
 		cout << "Number of characters: " << endl;
 		cout << i;
 
@@ -106,7 +125,8 @@ int i = 1, b = 1;
 		}
 		Infile.close();
 	
+		return 0;
 
-	return 0;
 }
+
 
