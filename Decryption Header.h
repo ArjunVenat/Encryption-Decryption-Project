d1464685b65b6a0a9d09c58b9d1b17532b9723f3
ArjunVenat <34625035+ArjunVenat@users.noreply.github.com>
2018-04-17 22:39:15 -0400

    Update Decryption Header.h

--- Decryption Header.h
+++ Decryption Header.h
@@ -1,133 +1,142 @@
-#pragma once
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
-#include <sstream>
-#include <string>
-#include <windows.h>
-
+#include "Decryption Header 2.h"
 using namespace std;
 
+void Decryption(int i, int ctm[3][700], int random[3][3]) {
+	int b = i;
+
+	float C[3][3], Adjoint[3][3], Inverse[3][3], Decoded[3][700];
+	float determinant[3][5];
+	float temp[2][2];
+	float determinantA;
+	char message[3][700];
+	string filename, sline;
+	int count = 0;
 
 
-void charconverter(int i, char storage[], int part2[], int y) {
-	//The element from the character array is stored into an integer array
 
-	for (int O = 1; O <= i; O++) {
 
-		if (O > (i - y)) {
-			for (int c = 1; c <= y; c++)
+	cout << "Key Matrix: \n" << endl;
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+		{
+			cout << random[i][j] << " ";
+			if (j == 2)
+				cout << endl;
+		}
+	cout << endl;
+
+	//Co-factor Matrix
+	cofactorMatrix(random, C, temp);
+
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 3; ++j)
+		{
+			Adjoint[i][j] = C[j][i];
+		}
+
+	//Determinant
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 5; ++j)
+		{
+			determinant[i][j] = static_cast<float> (random[i][j]);
+
+			if (j == 3)
+				determinant[i][j] = static_cast<float> (random[i][0]);
+			if (j == 4)
+				determinant[i][j] = static_cast<float> (random[i][1]);
+
+
+			
+		}
+	cout << endl;
+
+	//    | A | = a11a22a33 + a12a23a31 + a13a21a32 – a31a22a13 – a32a23a11 – a33a21a12
+	determinantA = determinant[0][0] * determinant[1][1] * determinant[2][2] +
+		determinant[0][1] * determinant[1][2] * determinant[2][3] +
+		determinant[0][2] * determinant[1][3] * determinant[2][4] -
+		determinant[2][0] * determinant[1][1] * determinant[0][2] -
+		determinant[2][1] * determinant[1][2] * determinant[0][3] -
+		determinant[2][2] * determinant[1][3] * determinant[0][4];
+
+
+	cout << endl;
+	cout << "\nInverse Matrix: \n" << endl;    //Inverse Matrix
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 3; ++j)
+		{
+			Inverse[i][j] = Adjoint[i][j] / determinantA;
+			cout << Inverse[i][j] << " ";
+			if (j == 2)
+				cout << "\n";
+		}
+
+	cout << endl;
+	
+//The inverse is multiplied by the ciphertext to get the message in the form of the numbers, which must be then converted into characters
+
+	for (int i = 0; i < (b / 3); i++)
+		for (int j = 0; j < 3; j++)
+		{
+			float a = 0;
+			for (int k = 0; k < 3; k++)
 			{
-				part2[O] = 0;
-				O++;
+				a = a + Inverse[j][k] * ctm[k][i];
 
-				//The Fillers are going to be 0
 			}
-			break;
-		}
-
-		if (storage[O] == '.')
-			part2[O] = 256;
-
-		else if (storage[O] == ' ')
-			part2[O] = 1;
-
-		else {
-			int a = part2[O];
-			int b = (part2[O]) - 64;
-
-			a = 64 + (2 * b);
-			part2[O] = a;
-		}
-		// The formula is 64 + 2((part 2 value) - 64)
-		// For example, a = 65 in ASCII. A becomes 64 + 2(65-64) or 66
-	}
-
-}
-
-
-
-
-
-void arrangement1x3(int i, int part2[], int ptm[7][300])
-{
-	//For loop which stores each 3 terms down by rows in a separate column of one matrix
-
-	cout << endl;
-
-
-	// "ptm" = Plain Text Matrix (1x3 matrices)
-
-	int h = 1;
-
-	for (int c = 0; c <= (i / 3); c++) {
-
-		for (int r = 1; r <= 3; r++) {
-			int a = part2[h];
-			ptm[r][c] = a;
-			h++;
-		}
-	}
-	for (int k = 1; k <= 3; k++) {
-		for (int l = 0; l < (i / 3); l++) {
-			cout << ptm[k][l] << "  " << "|" << "  ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	cout << endl;
-}
-
-
-
-
-void matrixmult(int i, int random[3][3], int ptm[7][300], int ctm[3][700])
-{
-
-	cout << "Cipher Text Matrix" << endl;
-	cout << endl;
-	for (int c = 0; c < (i / 3); c++) {
-
-		for (int w = 0; w < 3; w++) {
-			int a = 0;
-			int r = 1;
-			for (int q = 0; q < 3; q++) {
-
-				a = a + (random[w][q] * ptm[r][c]);
-				r++;
-			}
-
-			//Cipher Text Matrix  = "ctm"
-			ctm[w][c] = a;
-
-		}
-	}
-
-
-	for (int w = 0; w < 3; w++) {
-		for (int c = 0; c < i / 3; c++) {
-
-			if (ctm[w][c] > 0)
-				cout << " " << ctm[w][c] << "  ";
-
-			else
+			if (a < 0)
 			{
-				cout << ctm[w][c] << "  ";
+				a = 0;
 			}
 
+			Decoded[j][i] = a;
 		}
 
+	cout << endl;
 
-		cout << endl;
+	//Accuracy Issue
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < (b / 3); j++)
+		{
+			Decoded[i][j] = round(Decoded[i][j]);
+		}
 
-		cout << endl;
+	//Number to Character Conversion
+	for (int i = 0, a; i < 3; i++)
+		for (int j = 0; j < (b / 3); j++)
+		{
 
-	}
+			if (Decoded[i][j] == 256)
+				message[i][j] = '.';
+
+			else if (Decoded[i][j] == 1)
+				message[i][j] = ' ';
+
+			else {
+				a = static_cast<int> (Decoded[i][j]);
+
+				a -= 64;
+				a /= 2;
+				a += 64;
+
+				message[i][j] = a;
+
+				if (j == (b / 3) - 1)
+					cout << endl;
+
+				// The formula is 64 + 2((part 2 value) - 64)
+				// For example, a = 65 in ASCII. A becomes 64 + 2(65-64) or 66
+			}
+		}
+
+	cout << "\nMessage: " << endl;
+	for (int i = 0; i < (b / 3); i++)
+		for (int j = 0; j < 3; j++)
+		{
+			cout << message[j][i];
+		}
+	cout << endl;
+
 }
-
-
-void det(int random[3][3], int& d)
-{
-	d = random[0][0] * ((random[1][1] * random[2][2]) - (random[2][1] * random[1][2])) - random[0][1] * (random[1][0] * random[2][2] - random[2][0] * random[1][2]) + random[0][2] * (random[1][0] * random[2][1] - random[2][0] * random[1][1]);
-
-}
+	
+	
